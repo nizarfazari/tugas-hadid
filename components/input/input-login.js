144d18afc8f1e078b59047456a04db794924f473
nizarfazari <nexsel32@gmail.com>
2023-07-06 21:32:04 +0700

    add comment in input login

--- components/input/input-login.js
+++ components/input/input-login.js
@@ -1,4 +1,5 @@
 // ini adalah input login
+// testing dari sini masbroo
 const InputLogin = ({ type = "text", placeholder }) => {
   return <input type={type} className="w-[462px] rounded-[30px] pl-[20px] h-[50px] outline-none" placeholder={placeholder} />;
 };
